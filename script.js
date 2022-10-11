0d1f8562fe1007169f8d1e5628cd70b6c0fdac83
zakaria584 <zakariaibrahim20005@outlook.com>
2022-10-11 14:13:12 +0200

    y

---
+++ script.js
@@ -0,0 +1,14 @@
+alert("hallo wereld");
+
+document.getElementById("title").style.color = "red";
+
+var getal1 = 12;
+var getal2 = 4;
+var som = getal1 * getal2;
+alert(getal1 + " * " + getal2 + " = " + som);
+
+var kleur = prompt ("geef een engelse kleurnaam","");
+
+document.getElementById("title").style.color = kleur;
+
+document.getElementById("p1").innerHTML = "hello "+ kleur +"!";
